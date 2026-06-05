2dc86d6d401108dd7d821cfc790053ea7f5d97ae
Baileyd18 <163697338+Baileyd18@users.noreply.github.com>
2026-06-04 23:19:26 -0400

    Update Draft_App_fixed.py

--- Draft_App_fixed.py
+++ Draft_App_fixed.py
@@ -1012,7 +1012,7 @@ def calculate_results():
 
     final_score = (category_score * 0.60) + (star_power * 0.40)
 
-    wins = round(max(15, min(82, 27 + (final_score * 1))))
+    wins = round(max(15, min(82, 27 + (final_score * 1.1))))
     losses = 82 - wins
 
     if wins >= 82:
